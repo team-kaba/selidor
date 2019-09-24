0b6ba49a654d571938fe63fc1d56d99a0f796a83
ryotan <ryotan@ucb-jp.net>
2019-09-25 07:45:39 +0900

    :construction_worker: fix CircleCI env file

--- .ci/scripts/env.sh
+++ .ci/scripts/env.sh
@@ -22,9 +22,11 @@ export ALT_DEPLOYMENT_REPOSITORY
 if [ "${CI:-}" != "true" ]; then
   env_file="${PROJECT_ROOT_DIR}/.env.local"
 elif [ "${CIRCLECI}" == "true" ]; then
-  env_file="${PROJECT_ROOT_DIR}/.env"
+  env_file="${PROJECT_ROOT_DIR}/.circleci/.env"
 fi
 
 if [ -f "${env_file:-}" ]; then
   source "${env_file}"
+else
+  echo "'${env_file}' is not found."
 fi
