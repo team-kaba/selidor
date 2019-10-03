fe2858468cb5918e00aec4193cfd16904c31eab2
ryotan <ryotan@ucb-jp.net>
2019-10-03 19:35:31 +0900

    :bug: Extract only annotated tags as release tag

--- .ci/scripts/functions-git.sh
+++ .ci/scripts/functions-git.sh
@@ -3,5 +3,12 @@ function git_dirty_worktree() {
 }
 
 function git_current_version_tag() {
-  git show-ref --tags -d --abbrev=0 | cut -d' ' -f2 | sed -e 's!refs/tags/!!g' | grep '^v' | sort --version-sort --reverse --ignore-nonprinting | head -n 1
+  # Annotated tag だけを対象にするために `^{}` が付いたtagを対象としています。
+  git show-ref --tags -d --abbrev=0 |
+    cut -d' ' -f2 |
+    sed -e 's!refs/tags/!!g' |
+    grep -E '^v[0-9]+\.[0-9]+\.[0-9]+\^\{\}$' |
+    sed -e 's!\^{}$!!g' |
+    sort --version-sort --reverse --ignore-nonprinting |
+    head -n 1
 }
