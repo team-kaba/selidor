3d8f7a2af91635cfcee8195108cae6ced877ac8e
ryotan <ryotan@ucb-jp.net>
2019-11-19 15:14:00 +0900

    ⚒️: Set parent version explicitly if parent.version==${revision}.

--- .ci/scripts/maven-verify.sh
+++ .ci/scripts/maven-verify.sh
@@ -12,4 +12,5 @@ source "${script_dir}/source.sh"
 
 project_to_verify=${1:-.}
 
+set_parent_version_to_pom "$(get_pull_request_revision_from_pom)" "${project_to_verify}"
 mvnw clean verify -f "${project_to_verify}/pom.xml"
