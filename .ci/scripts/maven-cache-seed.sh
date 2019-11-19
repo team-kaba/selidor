a469d9d72f823e06b4cfe92f48ddebb17050d5ff
ryotan <ryotan@ucb-jp.net>
2019-11-20 03:31:55 +0900

    ⚒️: Include all pom.xml files in maven repository cache seed

--- .ci/scripts/maven-cache-seed.sh
+++ .ci/scripts/maven-cache-seed.sh
@@ -14,4 +14,4 @@ project_revision=$(get_project_revision_from_pom)
 revision="$(revision_for_current_build "${project_revision}")"
 
 echo "revision=${revision}"
-cat "${@}"
+find . -type f -name 'pom.xml' -exec bash -c 'f="${1}"; echo "${f#./}.sha256=$(xmlstarlet c14n --without-comments "${f}" | sha256sum | cut -d\  -f 1)"' _ {} \;
