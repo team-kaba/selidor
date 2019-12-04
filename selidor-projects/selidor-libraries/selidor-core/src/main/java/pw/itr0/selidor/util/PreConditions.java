5df5aef51b3605fce8e96c915b717b5450c678c4
ryotan <ryotan@ucb-jp.net>
2019-12-04 19:59:03 +0900

    ♻️: Remove unused parameter from internal method

--- selidor-projects/selidor-libraries/selidor-core/src/main/java/pw/itr0/selidor/util/PreConditions.java
+++ selidor-projects/selidor-libraries/selidor-core/src/main/java/pw/itr0/selidor/util/PreConditions.java
@@ -1,52 +1,32 @@
 package pw.itr0.selidor.util;
 
-import java.util.Objects;
 import java.util.function.Supplier;
 
 public abstract class PreConditions {
 
   public static void requireNonNull(Object object, Supplier<String> message) {
-    if (object == null) {
-      throw preConditionNotMet(message, () -> "object must not be null.");
-    }
+    require(object != null, message);
   }
 
   public static void requireExactSize(byte[] bytes, int expected, Supplier<String> message) {
-    final int actual = bytes.length;
-    if (actual != expected) {
-      throw preConditionNotMet(
-          message,
-          () ->
-              "byte array must be exactly " + expected + "bytes. actual size is [" + actual + "]");
-    }
+    require(bytes.length == expected, message);
   }
 
   public static void requireExactSize(String string, int expected, Supplier<String> message) {
-    final int actual = string.length();
-    if (Character.codePointCount(string, 0, actual) != expected) {
-      throw preConditionNotMet(
-          message,
-          () ->
-              "string must be exactly "
-                  + expected
-                  + " chars long. actual size is ["
-                  + actual
-                  + "]");
-    }
+    require(Character.codePointCount(string, 0, string.length()) == expected, message);
   }
 
   public static void requireLessThan(String string, String compare, Supplier<String> message) {
-    if (compare.compareTo(string) < 0) {
-      throw preConditionNotMet(
-          message, () -> "string must not exceed '" + compare + "'. string=[" + string + "]");
+    require(string.compareTo(compare) < 0, message);
+  }
+
+  private static void require(boolean condition, Supplier<String> message) {
+    if (!condition) {
+      throw preConditionNotMet(message);
     }
   }
 
-  private static IllegalArgumentException preConditionNotMet(
-      Supplier<String> messageSupplier, Supplier<String> defaultValue) {
-    return messageSupplier != null
-        ? new IllegalArgumentException(
-            Objects.requireNonNullElseGet(messageSupplier.get(), defaultValue))
-        : new IllegalArgumentException(defaultValue.get());
+  private static IllegalArgumentException preConditionNotMet(Supplier<String> messageSupplier) {
+    return new IllegalArgumentException(messageSupplier.get());
   }
 }
