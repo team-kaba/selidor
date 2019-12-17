a2a493fee2dc4b9b0b460ab5728713039e9b10ae
ryotan <ryotan@ucb-jp.net>
2019-12-18 07:03:28 +0900

    🐛(autoconfig): Fix to run successfully if HttpProxy.class is absent

--- selidor-projects/selidor-autoconfigure/src/main/java/pw/itr0/selidor/boot/autoconfigure/http/client/OkHttp3ClientAutoConfiguration.java
+++ selidor-projects/selidor-autoconfigure/src/main/java/pw/itr0/selidor/boot/autoconfigure/http/client/OkHttp3ClientAutoConfiguration.java
@@ -1,52 +1,15 @@
 package pw.itr0.selidor.boot.autoconfigure.http.client;
 
 import okhttp3.OkHttpClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.web.client.RestTemplateCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-import pw.itr0.selidor.http.client.okhttp3.OkHttp3PreemptiveProxyAuthenticator;
-import pw.itr0.selidor.http.proxy.HttpProxies;
-import pw.itr0.selidor.http.proxy.HttpProxy;
+import org.springframework.context.annotation.Import;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(OkHttpClient.class)
-public class OkHttp3ClientAutoConfiguration {
-  @Bean
-  @ConditionalOnClass(HttpProxies.class)
-  @ConditionalOnMissingBean
-  OkHttpClient okHttpClient(
-      @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") // たぶん、IntelliJのバグじゃないかと。
-          @Autowired(required = false)
-          HttpProxies proxies) {
-    final OkHttpClient.Builder builder = new OkHttpClient.Builder();
-    return proxies != null
-        ? builder
-            .proxySelector(proxies.getProxySelector())
-            .proxyAuthenticator(new OkHttp3PreemptiveProxyAuthenticator(proxies))
-            .build()
-        : builder.build();
-  }
-
-  @Bean
-  @ConditionalOnClass(HttpProxy.class)
-  @ConditionalOnBean(HttpProxies.class)
-  @ConditionalOnMissingBean
-  OkHttp3PreemptiveProxyAuthenticator okHttp3PreemptiveProxyAuthenticator(
-      @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") // たぶん、IntelliJのバグじゃないかと。
-          HttpProxies proxies) {
-    return new OkHttp3PreemptiveProxyAuthenticator(proxies);
-  }
-
-  @Bean
-  @ConditionalOnClass(RestTemplate.class)
-  RestTemplateCustomizer okHttpRestTemplate(OkHttpClient okHttpClient) {
-    return restTemplate ->
-        restTemplate.setRequestFactory(new OkHttp3ClientHttpRequestFactory(okHttpClient));
-  }
-}
+@Import({
+  OkHttp3ClientConfiguration.Direct.class,
+  OkHttp3ClientConfiguration.ProxyAware.class,
+  OkHttp3ClientConfiguration.SpringRestTemplate.class
+})
+public class OkHttp3ClientAutoConfiguration {}
---
+++ selidor-projects/selidor-autoconfigure/src/main/java/pw/itr0/selidor/boot/autoconfigure/http/client/OkHttp3ClientConfiguration.java
@@ -0,0 +1,69 @@
+package pw.itr0.selidor.boot.autoconfigure.http.client;
+
+import okhttp3.Authenticator;
+import okhttp3.OkHttpClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import pw.itr0.selidor.http.client.okhttp3.OkHttp3PreemptiveProxyAuthenticator;
+import pw.itr0.selidor.http.proxy.HttpProxies;
+
+abstract class OkHttp3ClientConfiguration {
+  @Configuration(proxyBeanMethods = false)
+  @ConditionalOnClass(RestTemplate.class)
+  static class SpringRestTemplate {
+    @Bean
+    @ConditionalOnBean(OkHttpClient.class)
+    @ConditionalOnMissingBean(OkHttp3ClientHttpRequestFactory.class)
+    RestTemplateCustomizer okHttpRestTemplate(
+        // @ConditionalOnBeanを付けているので問題ないはずですが、IntelliJ IDEAでは警告が出てしまうので抑止します。
+        @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+            OkHttpClient okHttpClient) {
+      return restTemplate ->
+          restTemplate.setRequestFactory(new OkHttp3ClientHttpRequestFactory(okHttpClient));
+    }
+  }
+
+  @Configuration(proxyBeanMethods = false)
+  @ConditionalOnMissingClass("pw.itr0.selidor.http.proxy.HttpProxies")
+  static class Direct {
+    @Bean
+    @ConditionalOnMissingBean
+    OkHttpClient okHttpClient() {
+      return new OkHttpClient.Builder().build();
+    }
+  }
+
+  @Configuration(proxyBeanMethods = false)
+  @ConditionalOnClass(HttpProxies.class)
+  static class ProxyAware {
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean(HttpProxies.class)
+    OkHttpClient proxiedOkHttpClient(HttpProxies proxies, Authenticator authenticator) {
+      return new OkHttpClient.Builder()
+          .proxySelector(proxies.getProxySelector())
+          .proxyAuthenticator(authenticator)
+          .build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean(HttpProxies.class)
+    Authenticator okhttp3PreemptiveProxyAuthenticator(HttpProxies proxies) {
+      return new OkHttp3PreemptiveProxyAuthenticator(proxies);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean({OkHttpClient.class, HttpProxies.class})
+    OkHttpClient nonProxiedOkHttpClient() {
+      return new OkHttpClient.Builder().build();
+    }
+  }
+}
