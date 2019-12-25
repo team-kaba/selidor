0ef76f833806cd498e109ff615cb32c9355d0e8d
ryotan <ryotan@ucb-jp.net>
2019-12-25 09:24:18 +0900

    ✅: Fix test failure on IntelliJ IDEA

--- selidor-tests/selidor-smoke-tests/src/test/java/pw/itr0/selidor/sandbox/app/SelidorSandboxApplicationTest.java
+++ selidor-tests/selidor-smoke-tests/src/test/java/pw/itr0/selidor/sandbox/app/SelidorSandboxApplicationTest.java
@@ -3,7 +3,7 @@ package pw.itr0.selidor.sandbox.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SelidorSandboxApplication.class)
 public class SelidorSandboxApplicationTest {
   @Test
   void contextRun() {}
